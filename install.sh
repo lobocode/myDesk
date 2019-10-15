e5e277ab1cc289142f087dde5f27109a37505f76
Vitor Lobo Ramos <lobocode@gmail.com>
2019-10-15 20:46:34 -0300

    up

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ echo -e "\n Install dependencies\n"
 
 # Fedora
 if [ $lsb_dist == "fedora" ]; then
-    sudo yum install "zsh rofi htop lxappearance orage pcmanfm openbox leafpad terminator tint2 nitrogen xlockmore clipit obconf xpad mpv xpdf shutter p7zip gimp inkscape"
+    sudo dnf install "zsh rofi htop lxappearance orage pcmanfm openbox leafpad terminator tint2 feh xlockmore clipit obconf xpad mpv xpdf shutter p7zip gimp inkscape lutris unar p7zip htop"
 fi
 
 # CentOs
@@ -18,28 +18,24 @@ if [ $lsb_dist == "centos" ]; then
 fi
 
 # I need to find packages, versions or alternatives of these for centos.
-#lxappearance
-#clipit
-#xpad
-#shutter
 
-if [ ! -z $"USER" ] ; then
-    echo -e "\n|----------------------------------------------------|\n"
-    echo -e "\n Install ohMyZsh\n"
-    sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-    echo -e "\n|----------------------------------------------------|\n"
-    echo -e "\n Install install powerline fonts\n"
-    git clone https://github.com/powerline/fonts
-    cd fonts
-    ./install.sh
-    echo -e "\n|----------------------------------------------------|\n"
-    echo -e "\n Install myDesk config\n"
-    \cp -a .config/* /home/$USER/.config/
-    echo -e "\n|----------------------------------------------------|\n"
-    echo -e "\n Install myDesk themes config\n"
-    \cp -a themes/* /home/$USER/.themes/
+#if [ ! -z $"USER" ] ; then
+#    echo -e "\n|----------------------------------------------------|\n"
+#    echo -e "\n Install ohMyZsh\n"
+#    sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+#    echo -e "\n|----------------------------------------------------|\n"
+#    echo -e "\n Install install powerline fonts\n"
+#    git clone https://github.com/powerline/fonts
+#    cd fonts
+#    ./install.sh
+#    echo -e "\n|----------------------------------------------------|\n"
+#    echo -e "\n Install myDesk config\n"
+#    \cp -a .config/* /home/$USER/.config/
+#    echo -e "\n|----------------------------------------------------|\n"
+#    echo -e "\n Install myDesk themes config\n"
+#    \cp -a themes/* /home/$USER/.themes/
 
-fi
+#fi
 
 # Download wallpapers
 #wget -c url > path
